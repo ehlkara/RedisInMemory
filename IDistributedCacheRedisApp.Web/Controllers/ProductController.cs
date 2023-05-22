092f7f847e1ee9992c3ba30072769baef7041c04
ehlkara <ehlullah.fb@gmail.com>
2023-05-22 14:56:38 +0300

    complex type cache

--- IDistributedCacheRedisApp.Web/Controllers/ProductController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductController.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using IDistributedCacheRedisApp.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 
 namespace IDistributedCacheRedisApp.Web.Controllers
 {
@@ -21,17 +24,42 @@ namespace IDistributedCacheRedisApp.Web.Controllers
             DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
 
             cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
-            _distributedCache.SetString("name", "ehlullah", cacheEntryOptions);
-            await _distributedCache.SetStringAsync("surname", "karakurt");
+
+            //_distributedCache.SetString("name", "ehlullah", cacheEntryOptions);
+            //await _distributedCache.SetStringAsync("surname", "karakurt");
+
+            Product product = new Product { Id = 1, Name = "Pen", Price = 100 };
+            Product product2 = new Product { Id = 2, Name = "Pen2", Price = 100 };
+            string jsonproduct = JsonConvert.SerializeObject(product);
+            string jsonproduct2 = JsonConvert.SerializeObject(product2);
+
+            Byte[] byteproduct = Encoding.UTF8.GetBytes(jsonproduct);
+
+            _distributedCache.Set("product:1", byteproduct);
+
+            //await _distributedCache.SetStringAsync("product:1", jsonproduct, cacheEntryOptions);
+            //await _distributedCache.SetStringAsync("product:2", jsonproduct2, cacheEntryOptions);
+
             return View();
         }
 
         public IActionResult Show()
         {
-            string name = _distributedCache.GetString("name");
-            string surname = _distributedCache.GetString("surname");
-            string fullname = name + surname;
-            ViewBag.fullname = fullname; 
+            //string name = _distributedCache.GetString("name");
+            //ViewBag.name = name;
+
+            //string jsonProduct = _distributedCache.GetString("product:1");
+
+            //Product p = JsonConvert.DeserializeObject<Product>(jsonProduct);
+
+            Byte[] byteProduct = _distributedCache.Get("product:1");
+
+            string jsonProduct = Encoding.UTF8.GetString(byteProduct);
+
+            Product p = JsonConvert.DeserializeObject<Product>(jsonProduct);
+
+            ViewBag.product = p;
+            
             return View();
         }
 
