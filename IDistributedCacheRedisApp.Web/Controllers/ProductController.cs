a92197c4d5e5df05fb9dd44c5c863e95c966ff67
ehlkara <ehlullah.fb@gmail.com>
2023-05-22 11:32:55 +0300

    save basic data in redis

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IDistributedCache _distributedCache;
+
+        public ProductController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            _distributedCache.SetString("name", "ehlullah", cacheEntryOptions);
+            await _distributedCache.SetStringAsync("surname", "karakurt");
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            string name = _distributedCache.GetString("name");
+            string surname = _distributedCache.GetString("surname");
+            string fullname = name + surname;
+            ViewBag.fullname = fullname; 
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("name");
+
+            return View();
+        }
+    }
+}
