31838af8c73ef74b1e852de011ad4d2ae47ea1e9
ehlkara <ehlullah.fb@gmail.com>
2023-05-22 11:10:39 +0300

    created project and downlading redis nuget package

---
+++ IDistributedCacheRedisApp.Web/Controllers/PoductsController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+    public class PoductsController : Controller
+    {
+        private IDistributedCache _distributedCache;
+
+        public PoductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IDistributedCacheRedisApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IDistributedCacheRedisApp.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
