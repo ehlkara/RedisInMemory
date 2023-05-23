7d327c7bd796e00d7771d978b09fe6e8c9caa97c
ehlkara <ehlullah.fb@gmail.com>
2023-05-23 11:15:27 +0300

    created project and connect to redis and than Redis String methods

---
+++ RedisExchangeAPI.Web/Controllers/StringTypeController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class StringTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+
+        private readonly IDatabase db;
+
+        public StringTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(0);
+        }
+
+        public IActionResult Index()
+        {
+            db.StringSet("name", "Ehlullah Karakurt");
+            db.StringSet("ziyaretçi", 100);
+
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            //var value = db.StringGet("name");
+
+            //db.StringIncrement("ziyaretçi", 10);
+
+            //var count = db.StringDecrementAsync("ziyaretçi", 1).Result;
+
+            //var value = db.StringGetRange("name", 0, 3);
+
+            var value = db.StringLength("name");
+
+            db.StringDecrementAsync("ziyaretçi", 10).Wait();
+
+            //if (value.HasValue)
+            //{
+            //    ViewBag.value = value.ToString();
+            //}
+
+            ViewBag.value = value.ToString();
+
+            return View();
+        }
+    }
+}
---
+++ RedisExchangeAPI.Web/Program.cs
@@ -0,0 +1,35 @@
+﻿using RedisExchangeAPI.Web.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSingleton<RedisService>();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+var redisService = app.Services.GetService<RedisService>();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+redisService.Connect();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
