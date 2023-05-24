efcd919eac4919415007f0ad8dfcc9de3662a3bc
ehlkara <ehlullah.fb@gmail.com>
2023-05-24 15:03:18 +0300

    created project

---
+++ SharpRedis.API/Models/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SharpRedis.API.Models
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+			//Seed
+			modelBuilder.Entity<Product>().HasData(
+				new Product() { Id = 1, Name = "Pen 1", Price = 100 },
+				new Product() { Id = 2, Name = "Pen 2", Price = 200 },
+				new Product() { Id = 3, Name = "Pen 3", Price = 300 }
+				);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
